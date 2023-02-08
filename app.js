baa2966d39e277c66e2f63db54fdcadd6e665c28
feruzu <fernanda23demarzo@gmail.com>
2023-02-07 21:28:48 -0300

    js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-
 // Variables botones
 let anterior = document.querySelector(".anterior");
 let play = document.querySelector(".play");
 let pausa = document.querySelector(".pausa");
 let siguiente = document.querySelector(".siguiente");
 let duracion = document.querySelector("#duracion-cancion");
+
+
+
+
+function reproducir(){
+    console.log('reproduciendo');
+}
 
 

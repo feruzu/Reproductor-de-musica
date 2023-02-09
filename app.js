3e3764dfc76ba579f8dbecb789f4e3f4424804be
feruzu <fernanda23demarzo@gmail.com>
2023-02-09 16:59:51 -0300

    Play y pause

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ let play = document.querySelector(".play");
 let pausa = document.querySelector(".pausa");
 let siguiente = document.querySelector(".siguiente");
 let duracion = document.querySelector("#duracion-cancion");
-
+var audio = document.getElementById("audio");
 
 
 
 function reproducir(){
     console.log('reproduciendo');
+    var audio = document.getElementById("audio");
+    audio.play();
+    document.getElementById("play").style.display="none";
+    document.getElementById("pausa").style.display="block";
 }
 
+function pause(){
+    var audio = document.getElementById("audio");
+    audio.pause(); 
+    console.log('reproduciendo');
+    document.getElementById("play").style.display="block";
+    document.getElementById("pausa").style.display="none";
+    
+}
 

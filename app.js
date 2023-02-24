1ce69299f8af360e992e5003232e4fcf3b0c9dda
feruzu <fernanda23demarzo@gmail.com>
2023-02-24 15:17:31 -0300

    Organizando variables

--- app.js
+++ app.js
@@ -6,51 +6,45 @@ let siguiente = document.querySelectorAll(".siguiente");
 let duracion = document.querySelector("#duracion-cancion");
 let portada = document.querySelector(".portada");
 
-var audio1 = document.getElementById("audio1");
-var audio2 = document.getElementById("audio2");
-
+let audio1 = document.getElementById("audio1");
+let audio2 = document.getElementById("audio2");
+let audio3 = document.getElementById("audio3");
 
 
 
 
 
 function reproducir1(){
-    var audio1 = document.getElementById("audio1");
     audio1.play();
     document.getElementById("play1").style.display="none";
     document.getElementById("pausa1").style.display="block";
 }
 
 function pause1(){
-    var audio1 = document.getElementById("audio1");
     audio1.pause(); 
     document.getElementById("play1").style.display="block";
     document.getElementById("pausa1").style.display="none";  
 }
 
 function reproducir2(){
-  var audio2 = document.getElementById("audio2");
   audio2.play();
   document.getElementById("play2").style.display="none";
   document.getElementById("pausa2").style.display="block";
 }
 
 function pause2(){
-  var audio2 = document.getElementById("audio2");
   audio2.pause(); 
   document.getElementById("play2").style.display="block";
   document.getElementById("pausa2").style.display="none"; 
 }
 
 function reproducir3(){
-  var audio3 = document.getElementById("audio3");
   audio3.play();
   document.getElementById("play3").style.display="none";
   document.getElementById("pausa3").style.display="block";
 }
 
 function pause3(){
-  var audio3 = document.getElementById("audio3");
   audio3.pause(); 
   document.getElementById("play3").style.display="block";
   document.getElementById("pausa3").style.display="none"; 

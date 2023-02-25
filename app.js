fc7f87716f0925690b2cc11680b191d0baf29d94
feruzu <fernanda23demarzo@gmail.com>
2023-02-25 11:16:55 -0300

    Cambios en var swiper

--- app.js
+++ app.js
@@ -10,89 +10,68 @@ let audio1 = document.getElementById("audio1");
 let audio2 = document.getElementById("audio2");
 let audio3 = document.getElementById("audio3");
 
-
-
-
-
-function reproducir1(){
-    audio1.play();
-    document.getElementById("play1").style.display="none";
-    document.getElementById("pausa1").style.display="block";
+function reproducir1() {
+  audio1.play();
+  document.getElementById("play1").style.display = "none";
+  document.getElementById("pausa1").style.display = "block";
 }
 
-function pause1(){
-    audio1.pause(); 
-    document.getElementById("play1").style.display="block";
-    document.getElementById("pausa1").style.display="none";  
+function pause1() {
+  audio1.pause();
+  document.getElementById("play1").style.display = "block";
+  document.getElementById("pausa1").style.display = "none";
 }
 
-function reproducir2(){
+function reproducir2() {
   audio2.play();
-  document.getElementById("play2").style.display="none";
-  document.getElementById("pausa2").style.display="block";
+  document.getElementById("play2").style.display = "none";
+  document.getElementById("pausa2").style.display = "block";
 }
 
-function pause2(){
-  audio2.pause(); 
-  document.getElementById("play2").style.display="block";
-  document.getElementById("pausa2").style.display="none"; 
+function pause2() {
+  audio2.pause();
+  document.getElementById("play2").style.display = "block";
+  document.getElementById("pausa2").style.display = "none";
 }
 
-function reproducir3(){
+function reproducir3() {
   audio3.play();
-  document.getElementById("play3").style.display="none";
-  document.getElementById("pausa3").style.display="block";
+  document.getElementById("play3").style.display = "none";
+  document.getElementById("pausa3").style.display = "block";
 }
 
-function pause3(){
-  audio3.pause(); 
-  document.getElementById("play3").style.display="block";
-  document.getElementById("pausa3").style.display="none"; 
+function pause3() {
+  audio3.pause();
+  document.getElementById("play3").style.display = "block";
+  document.getElementById("pausa3").style.display = "none";
 }
 
+// Cambiar canción
 
-// Siguiente
+function otraCancion() {
+  audio1.currentTime = 0;
+  audio1.pause();
+  document.getElementById("play1").style.display = "block";
+  document.getElementById("pausa1").style.display = "none";
 
-function otraCancion(){
-    audio1.currentTime = 0;
-    audio1.pause();
-    document.getElementById("play1").style.display="block";
-    document.getElementById("pausa1").style.display="none"; 
+  audio2.currentTime = 0;
+  audio2.pause();
+  document.getElementById("play2").style.display = "block";
+  document.getElementById("pausa2").style.display = "none";
 
-    audio2.currentTime = 0;
-    audio2.pause();
-    document.getElementById("play2").style.display="block";
-    document.getElementById("pausa2").style.display="none"; 
-
-    audio3.currentTime = 0;
-    audio3.pause();
-    document.getElementById("play3").style.display="block";
-    document.getElementById("pausa3").style.display="none";
+  audio3.currentTime = 0;
+  audio3.pause();
+  document.getElementById("play3").style.display = "block";
+  document.getElementById("pausa3").style.display = "none";
 }
-
-
-
-
 
 
 
 // Carousel
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    slidesPerGroup: 1,
-    loop: true,
-    centerSlide: "true",
-    grabCursor: "true",
-    fade: "true",
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".siguiente",
-      prevEl: ".anterior",
-    },
-  
-
-  });
+var swiper = new Swiper(".swiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".siguiente",
+    prevEl: ".anterior",
+  },
+});

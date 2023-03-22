72a11ba43806c81823e4a87ddee2dcd3a887dee4
feruzu <fernanda23demarzo@gmail.com>
2023-03-22 14:28:15 -0300

    Limpieza

--- app.js
+++ app.js
@@ -6,31 +6,6 @@ let audio1 = document.getElementById("audio1");
 let audio2 = document.getElementById("audio2");
 let audio3 = document.getElementById("audio3");
 
-function reproducir1() {
-  audio1.play();
-  document.getElementById("play1").style.display = "none";
-  document.getElementById("pausa1").style.display = "block";
-}
-
-function pause1() {
-  audio1.pause();
-}
-
-function reproducir2() {
-  audio2.play();
-}
-
-function pause2() {
-  audio2.pause();
-}
-
-function reproducir3() {
-  audio3.play();
-}
-
-function pause3() {
-  audio3.pause();
-}
 
 // Cambiar canción
 function otraCancion() {
@@ -44,6 +19,7 @@ function otraCancion() {
   audio3.pause();
 }
 
+
 // Carousel
 var swiper = new Swiper(".swiper", {
   cssMode: true,
@@ -51,4 +27,6 @@ var swiper = new Swiper(".swiper", {
     nextEl: ".siguiente",
     prevEl: ".anterior",
   },
+
 });
+

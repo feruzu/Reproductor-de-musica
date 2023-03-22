516143c367aec463167b33a1d1f002f9f2738604
feruzu <fernanda23demarzo@gmail.com>
2023-03-22 14:53:07 -0300

    Commit

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ let audio1 = document.getElementById("audio1");
 let audio2 = document.getElementById("audio2");
 let audio3 = document.getElementById("audio3");
 
-
 // Cambiar canción
 function otraCancion() {
   audio1.currentTime = 0;
@@ -17,8 +16,33 @@ function otraCancion() {
 
   audio3.currentTime = 0;
   audio3.pause();
+
+  noReproducirVaias();
 }
 
+// No reproducir varias canciones al mismo tiempo
+function noReproducirVaias() {
+  if (audio1) {
+    audio2.currentTime = 0;
+    audio2.pause();
+
+    audio3.currentTime = 0;
+    audio3.pause();
+  }
+  if (audio2) {
+    audio1.currentTime = 0;
+    audio1.pause();
+
+    audio3.currentTime = 0;
+    audio3.pause();
+  } else if (audio3) {
+    audio1.currentTime = 0;
+    audio1.pause();
+
+    audio2.currentTime = 0;
+    audio2.pause();
+  }
+}
 
 // Carousel
 var swiper = new Swiper(".swiper", {
@@ -27,6 +51,4 @@ var swiper = new Swiper(".swiper", {
     nextEl: ".siguiente",
     prevEl: ".anterior",
   },
-
 });
-

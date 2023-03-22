ffc2cc093f92c7a01d70e6fba83cf5be340fa12a
feruzu <fernanda23demarzo@gmail.com>
2023-03-21 22:31:22 -0300

    js

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 // Variables botones
 let anterior = document.querySelectorAll(".anterior");
-let play = document.querySelector(".play");
-let pausa = document.querySelector(".pausa");
 let siguiente = document.querySelectorAll(".siguiente");
 
 let audio1 = document.getElementById("audio1");
 let audio2 = document.getElementById("audio2");
 let audio3 = document.getElementById("audio3");
-
 
 function reproducir1() {
   audio1.play();
@@ -17,54 +14,35 @@ function reproducir1() {
 
 function pause1() {
   audio1.pause();
-  document.getElementById("play1").style.display = "block";
-  document.getElementById("pausa1").style.display = "none";
 }
 
 function reproducir2() {
   audio2.play();
-  document.getElementById("play2").style.display = "none";
-  document.getElementById("pausa2").style.display = "block";
 }
 
 function pause2() {
   audio2.pause();
-  document.getElementById("play2").style.display = "block";
-  document.getElementById("pausa2").style.display = "none";
 }
 
 function reproducir3() {
   audio3.play();
-  document.getElementById("play3").style.display = "none";
-  document.getElementById("pausa3").style.display = "block";
 }
 
 function pause3() {
   audio3.pause();
-  document.getElementById("play3").style.display = "block";
-  document.getElementById("pausa3").style.display = "none";
 }
 
 // Cambiar canción
-
 function otraCancion() {
   audio1.currentTime = 0;
   audio1.pause();
-  document.getElementById("play1").style.display = "block";
-  document.getElementById("pausa1").style.display = "none";
 
   audio2.currentTime = 0;
   audio2.pause();
-  document.getElementById("play2").style.display = "block";
-  document.getElementById("pausa2").style.display = "none";
 
   audio3.currentTime = 0;
   audio3.pause();
-  document.getElementById("play3").style.display = "block";
-  document.getElementById("pausa3").style.display = "none";
 }
-
-
 
 // Carousel
 var swiper = new Swiper(".swiper", {

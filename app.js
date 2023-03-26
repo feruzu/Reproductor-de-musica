900a4740285b41d4d1c9700c31fa2244f9050811
feruzu <fernanda23demarzo@gmail.com>
2023-03-26 14:19:27 -0300

    Mejore funcionamiento de etiqueta audio

--- app.js
+++ app.js
@@ -6,6 +6,33 @@ let audio1 = document.getElementById("audio1");
 let audio2 = document.getElementById("audio2");
 let audio3 = document.getElementById("audio3");
 
+let audio = document.querySelectorAll("audio.audio");
+
+// Etiqueta Audio, detener canciom al reproducir otra etiqueta audio
+
+audio[0].addEventListener("play", () => {
+  audio2.currentTime = 0;
+  audio2.pause();
+
+  audio3.currentTime = 0;
+  audio3.pause();
+});
+
+audio[1].addEventListener("play", () => {
+  audio1.currentTime = 0;
+  audio1.pause();
+
+  audio3.currentTime = 0;
+  audio3.pause();
+});
+
+audio[2].addEventListener("play", () => {
+  audio1.currentTime = 0;
+  audio1.pause();
+
+  audio2.currentTime = 0;
+  audio2.pause();
+});
 
 // Cambiar canción
 function otraCancion() {
@@ -19,10 +46,9 @@ function otraCancion() {
   audio3.pause();
 }
 
-
 // Carousel
 var swiper = new Swiper(".swiper", {
-  cssMode: true,
+  cssMode: false,
   navigation: {
     nextEl: ".siguiente",
     prevEl: ".anterior",
